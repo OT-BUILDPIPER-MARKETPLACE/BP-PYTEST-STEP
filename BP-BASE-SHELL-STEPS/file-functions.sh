af5beb24821ab6894617acc5ec7f9ebeaa7dc999
yuvrajsingh-opstree <71yuvrajsingh6123@gmail.com>
2025-11-17 12:39:17 +0530

    Clean submodule residue

--- BP-BASE-SHELL-STEPS/file-functions.sh
+++
@@ -1,9 +0,0 @@
-function isFileExist() {
-    FILEPATH=$1
-
-    if [ -f "$FILEPATH" ]; then
-        echo 0
-    else
-        echo 1
-    fi
-}
--- BP-BASE-SHELL-STEPS/str-functions.sh
+++
@@ -1,8 +0,0 @@
-function isStrNonEmpty() {
-    STR=$1
-    if [ -z "$STR" ]; then
-        echo 1
-    else
-        echo 0
-    fi
-}
